a18b61c862377102a300d0cc1251b4e4b60b5e56
ninad-1234 <67185579+ninad-1234@users.noreply.github.com>
2021-10-09 09:10:38 +0530

    Update user_creation.go

--- user_creation/user_creation.go
+++ user_creation/user_creation.go
@@ -1,1 +1,67 @@
 
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"reflect"
+	"time"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type MongoField struct {
+	ID  string `json: "id"`
+	Name  string    `json: "name"`
+	Email string   `json: "email"`
+	Password string   `json: "psw"`
+}
+
+func server(userDB MongoField){
+	const url ="http://localhost:8000/post"
+	var  byte  = []byte(userDB.Name)
+	req, err := http.NewRequest("POST", url, byte)
+}
+
+func main() {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	fmt.Println("ClientOptopm TYPE:", reflect.TypeOf(clientOptions), "\n")
+
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		fmt.Println("Mongo.connect() ERROR: ", err)
+		os.Exit(1)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 15*time.Second)
+
+	col := client.Database("Instagram_Database").Collection("User COllection")
+	fmt.Println("Collection Type: ", reflect.TypeOf(col), "\n")
+
+	userDB := MongoField{
+		ID:  "ninad0104",
+		Name:  "Ninad Topale",
+		Email: "topaleninad01@gmail.com",
+		Password: "nt2001$",
+	}
+
+	fmt.Println("oneDoc Type: ", reflect.TypeOf(userDB), "\n")
+
+	result, insertErr := col.InsertOne(ctx, userDB)
+	if insertErr != nil {
+		fmt.Println("InsertONE Error:", insertErr)
+		os.Exit(1)
+	} else {
+		fmt.Println("InsertOne() result type: ", reflect.TypeOf(result))
+		fmt.Println("InsertOne() api result type: ", result)
+
+		newID := result.InsertedID
+		fmt.Println("InsertedOne(), newID", newID)
+		fmt.Println("InsertedOne(), newID type:", reflect.TypeOf(newID))
+
+	}
+}
